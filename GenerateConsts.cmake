0e5c89aeae5e9dc6829d17c3d72a204955c7dd6b
Павел Карев <karevpavel133@gmail.com>
2021-10-21 20:09:01 +0300

    Some fixes

--- GenerateConsts.cmake
+++ GenerateConsts.cmake
@@ -1,15 +1,15 @@
 
-find_program(PYTHON_EXECUTABLE python3)
+find_program(PYTHON_EXECUTABLE python)
 if(NOT PYTHON_EXECUTABLE)
-	message(FATAL_ERROR "python3 not found. Required by generator.")
+	message(FATAL_ERROR "python not found. Required by generator.")
 endif()
 set(GENERATOR generate_resources.py)
 
 function(add_generated_stuff MAIN_DIR TARGET INFILE)
 	set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${INFILE})
-
 	set(GENERATOR_PATH ${PYTHON_EXECUTABLE} ${MAIN_DIR}/${GENERATOR})
 	set(INPATH ${MAIN_DIR}/${INFILE})
+
 	execute_process(
 			COMMAND ${GENERATOR_PATH} --print-dependencies ${INPATH}
 			OUTPUT_VARIABLE DEPENDENCIES
@@ -19,7 +19,7 @@ function(add_generated_stuff MAIN_DIR TARGET INFILE)
 	if (NOT RETURN_VALUE EQUAL 0)
 		message(FATAL_ERROR "Failed to get dependencies for ${INFILE}")
 	endif()
-	 message('DEPENDENCIES '${DEPENDENCIES})
+    message('DEPENDENCIES '${DEPENDENCIES})
 	add_custom_command(
 			COMMAND ${GENERATOR_PATH} ${INPATH}
 			OUTPUT ${DEPENDENCIES}
